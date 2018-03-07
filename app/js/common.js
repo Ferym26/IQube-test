91068cad1045605160c78ca3c0ba6cedf5469e2b
Ferym26 <ferym26@gmail.com>
2018-03-07 13:11:47 +0300

    test

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,24 @@ $(document).ready(function() {
 	//фикс svg спрайта для ИЕ
 	// svg4everybody();
 
+    ymaps.ready(init);
+    var myMap;
+
+    function init(){     
+
+        var myMap = new ymaps.Map('map', {
+			center: [52.45209395, 30.96043722],
+            zoom: 15,
+			controls: []
+
+		},
+
+		{suppressMapOpenBlock: true})
+
+
+
+    }
+
 
 
 });
