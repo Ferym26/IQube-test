47d8e919ba75de2e61d39fcac158ae958707c613
Ferym26 <ferym26@gmail.com>
2018-03-11 23:04:56 +0300

    done

--- app/js/common.js
+++ app/js/common.js
@@ -5,9 +5,10 @@ $(document).ready(function() {
 	//фикс svg спрайта для ИЕ
 	// svg4everybody();
 
+
+	//Яндекс карта
     ymaps.ready(init);
     var myMap;
-
     function init(){     
 
         var myMap = new ymaps.Map('map', {
@@ -16,13 +17,52 @@ $(document).ready(function() {
 			controls: []
 
 		},
-
 		{suppressMapOpenBlock: true})
-
-
-
     }
 
+
+
+    //сворачивалка тарифы в меню
+    $(".tariff__title").on("click", function() {
+    	$(".tariff__list").toggle(200);
+    	$(".tariff__title-arrow").toggleClass("is-bottom")
+    });
+
+
+    //свитчер карта/список водителей
+    var butnMap = $(".js_icon-map");
+    var butnDriversList = $(".js_icon-list");
+    var mapBox = $(".map-wrap");
+    var driversBox = $(".main-line__drivers-list");
+    //.is-visible .is-hidden
+
+    butnMap.on("click", function() {
+
+    	mapBox.addClass("is-visible");
+    	mapBox.removeClass("is-hidden");
+
+    	driversBox.addClass("is-hidden");
+    	driversBox.removeClass("is-visible");
+
+    	//это костылы только для теста. скрывает слой с иконками машин на карте
+    	$(".main-line__ui").addClass("is-visible");
+    	$(".main-line__ui").removeClass("is-hidden");
+
+    });
+
+    butnDriversList.on("click", function() {
+
+		mapBox.addClass("is-hidden");
+		mapBox.removeClass("is-visible");
+
+    	driversBox.addClass("is-visible");
+    	driversBox.removeClass("is-hidden");
+
+    	//это костылы только для теста. скрывает слой с иконками машин на карте
+    	$(".main-line__ui").addClass("is-hidden");
+    	$(".main-line__ui").removeClass("is-visible");
+
+    });
 
 
 });
